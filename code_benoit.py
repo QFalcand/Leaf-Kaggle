b571f6489878ec04f4e01182c616499aaf7981b9
QFalcand <qmsn@falcand.nom.fr>
2017-02-18 18:19:47 +0100

    Update code_benoit.py

--- code_benoit.py
+++ code_benoit.py
@@ -222,7 +222,7 @@ def generation_train_test(X, Y):
     '''Génère un ensemble de test et un un ensemble d'apprentissage à partir
     d'un échantillon donné en entrée, avec conservation du pourcentage d'exemples
     de chaque classe. Ensemble de test = 10% du départ'''
-    
+    #là tu n'as besoin de faire qu'un seul split, pas 10. Pour les 10% c'est l'attribut test_size
     skf = StratifiedShuffleSplit(n_splits = 1, test_size=0.1)
     
     list_ind = list(skf.split(X, Y))
