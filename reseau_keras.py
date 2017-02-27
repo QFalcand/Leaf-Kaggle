7d9064e99497362e4673517fda8532beb5bf340d
QFalcand <qmsn@falcand.nom.fr>
2017-02-27 15:02:47 +0100

    Add files via upload

--- reseau_keras.py
+++ reseau_keras.py
@@ -15,6 +15,7 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.preprocessing import scale
 import numpy as np
 import matplotlib.pyplot as plt
 from math import sqrt
@@ -76,10 +77,10 @@ def chargement_images(ids, input_shape):
     return np.array(images)
 
 
-str_reseau = 'RNN'
+str_reseau = 'histpur_RNN_norm'
 
 #on charge les données
-train = pd.read_csv('train.csv')
+train = pd.read_csv('train_norm.csv')
 
 t = train.as_matrix()
 #id de l'ensemble d'apprentissage
@@ -89,10 +90,13 @@ if 'CNN' not in str_reseau:
     X = t[:,2:]
 #    print(t.shape, X.shape)
 if 'hist' in str_reseau:
-    hist = pd.read_csv('hist.csv', header = None)
+    hist = pd.read_csv('hist_norm.csv', header = None)
     h = hist.as_matrix()
     id2 = ids - 1
-    X = h[id2.tolist(), 1:]
+#    X = h[id2.tolist(), 1:]
+    X2 = h[id2.tolist(), 1:]
+    X = np.concatenate((X, X2), axis = 1)
+#    print(c.shape)
 #objectif
 Y = t[:,1]
 #on recode Y pour avoir des labels qui ne soient plus textuels
@@ -101,7 +105,7 @@ Y = to_categorical(Y)
 
 if "CNN" in str_reseau:
     #on récupère les images
-    images = chargement_images(ids, (100, 100))
+    X = chargement_images(ids, (100, 100))
     
     #on prépare la génération de données supplémentaires
     #on s'autorise des rotations de 10degrés, des symmétires verticales et horizontales 
@@ -114,45 +118,51 @@ if "CNN" in str_reseau:
 Nb_splits = 1
 skf = StratifiedShuffleSplit(n_splits=Nb_splits)
 #on fixe le nombre d'époques d'apprentissage et le pas de visualisation
-max_epoch = 10000
-#    epoch_iter = 1
-#    
+max_epoch = 200
+
+liste_hist = []
 #pour chaque séparation test/apprentissage
 for train_index, test_index in skf.split(X, Y):
-    #on apprend un réseau
-    reseau = Sequential()
-    if 'CNN' in str_reseau:
-#        reseau.add(Reshape((1, 100, 100), input_shape=(100, 100, 1)))
-        reseau.add(Convolution2D(10, 5, 5, input_shape=images[0, :, :, :].shape,
-                                 border_mode='valid'))
-        reseau.add(Activation('relu'))
-        reseau.add(MaxPooling2D(pool_size=(2, 2)))
-#        self.add(Dropout(0.5))
-        reseau.add(Convolution2D(15, 5, 5))
-        reseau.add(Activation('relu'))
-        reseau.add(MaxPooling2D(pool_size=(2, 2)))
-        reseau.add(Dropout(0.5))
-        reseau.add(Flatten())
-#        reseau.add(Dense(99))
-#        reseau.add(Activation('softmax'))
-    elif 'RNN' in str_reseau:
-        reseau.add(Dense(100, input_dim = X.shape[1]))
-        reseau.add(Activation('relu'))
-    
-    #couches communes à tous les réseaux
-    reseau.add(Dense(99))
-    reseau.add(Activation('softmax'))
-    reseau.compile(loss='categorical_crossentropy', optimizer='rmsprop',
-                   metrics = ['categorical_accuracy', 'precision',
-                              'recall', 'fmeasure'])
+    #on essaie de récupérer le réseau appris aupparavant s'il existe
+    try:
+        reseau = load_model(str_reseau + '.h5')
+    except :
+        #on apprend un réseau
+        reseau = Sequential()
+        if 'CNN' in str_reseau:
+    #        reseau.add(Reshape((1, 100, 100), input_shape=(100, 100, 1)))
+            reseau.add(Convolution2D(10, 5, 5, input_shape=X[0, :, :, :].shape,
+                                     border_mode='valid'))
+            reseau.add(Activation('relu'))
+            reseau.add(MaxPooling2D(pool_size=(2, 2)))
+    #        self.add(Dropout(0.5))
+            reseau.add(Convolution2D(15, 5, 5))
+            reseau.add(Activation('relu'))
+            reseau.add(MaxPooling2D(pool_size=(2, 2)))
+            reseau.add(Dropout(0.5))
+            reseau.add(Flatten())
+    #        reseau.add(Dense(99))
+    #        reseau.add(Activation('softmax'))
+        elif 'RNN' in str_reseau:
+            reseau.add(Dense(100, input_dim = X.shape[1]))
+            reseau.add(Activation('relu'))
+#            reseau.add(Dense(100))
+#            reseau.add(Activation('relu'))
+        
+        #couches communes à tous les réseaux
+        reseau.add(Dense(99))
+        reseau.add(Activation('softmax'))
+        reseau.compile(loss='categorical_crossentropy', optimizer='rmsprop',
+                       metrics = ['categorical_accuracy', 'precision',
+                                  'recall', 'fmeasure'])
     
     #on récupère les données correspondantes
     X_train, X_test = X[train_index], X[test_index]
     Y_train, Y_test = Y[train_index], Y[test_index]
     if 'CNN' in str_reseau:
-        img_train, img_test = images[train_index], images[test_index]
+#        img_train, img_test = images[train_index], images[test_index]
         #on récupère les images via l'augmenteur de données
-        iterateur = idg.flow(img_train, Y_train)
+        iterateur = idg.flow(X_train, Y_train)
 
     print("Début d'apprentissage......")
 #        for i in range(0, max_epoch, epoch_iter):
@@ -163,22 +173,31 @@ for train_index, test_index in skf.split(X, Y):
                                           samples_per_epoch = Y_train.shape[0],
                                           nb_epoch = max_epoch, 
                                           verbose = 1, 
-                                          validation_data = (img_test, Y_test))
+                                          validation_data = (X_test, Y_test))
     else:
         historique = reseau.fit(X_train, Y_train, verbose = 0, nb_epoch = max_epoch, 
                                 validation_data = (X_test, Y_test))
-
+    liste_hist.append(historique.history)
     print("Fin d'apprentissage")
-results = {'Apprentissage':{'Accuracy':historique.history['categorical_accuracy'], 
-                    'F mesure':historique.history['fmeasure'], 
-                    'Précision':historique.history['precision'], 
-                    'Rappel':historique.history['recall'], 
-                    'Perte':historique.history['loss']},
-           'Validation':{'Accuracy':historique.history['val_categorical_accuracy'], 
-                    'F mesure':historique.history['val_fmeasure'], 
-                    'Précision':historique.history['val_precision'], 
-                    'Rappel':historique.history['val_recall'], 
-                    'Perte':historique.history['val_loss']}}
-graphique('CNN', results)
+
+init = [0] * max_epoch
+results = {'Apprentissage':{'Accuracy':init.copy(), 'F mesure':init.copy(), 
+                            'Précision':init.copy(), 'Rappel':init.copy(),
+                            'Perte':init.copy()},
+           'Validation':{'Accuracy':init.copy(), 'F mesure':init.copy(),
+                         'Précision':init.copy(), 'Rappel':init.copy(), 
+                         'Perte':init.copy()}}
+for elem in liste_hist:
+    results['Apprentissage']['Accuracy'] = [x + y / Nb_splits for x, y in zip(results['Apprentissage']['Accuracy'], elem['categorical_accuracy'])]
+    results['Apprentissage']['F mesure']  = [x + y / Nb_splits for x, y in zip(results['Apprentissage']['F mesure'], elem['fmeasure'])]
+    results['Apprentissage']['Précision'] = [x + y / Nb_splits for x, y in zip(results['Apprentissage']['Précision'], elem['precision'])]
+    results['Apprentissage']['Rappel'] = [x + y / Nb_splits for x, y in zip(results['Apprentissage']['Rappel'], elem['recall'])]
+    results['Apprentissage']['Perte'] = [x + y / Nb_splits for x, y in zip(results['Apprentissage']['Perte'], elem['loss'])]
+    results['Validation']['Accuracy'] = [x + y / Nb_splits for x, y in zip(results['Validation']['Accuracy'], elem['val_categorical_accuracy'])]
+    results['Validation']['F mesure'] = [x + y / Nb_splits for x, y in zip(results['Validation']['F mesure'], elem['val_fmeasure'])]
+    results['Validation']['Précision'] = [x + y / Nb_splits for x, y in zip(results['Validation']['Précision'], elem['val_precision'])]
+    results['Validation']['Rappel'] = [x + y / Nb_splits for x, y in zip(results['Validation']['Rappel'], elem['val_recall'])]
+    results['Validation']['Perte'] = [x + y / Nb_splits for x, y in zip(results['Validation']['Perte'], elem['val_loss'])]
+graphique(str_reseau, results)
 reseau.save(str_reseau + '.h5')
 print('réseau enregistré')
